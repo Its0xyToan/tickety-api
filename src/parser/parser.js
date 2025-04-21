1258bf3ff3318ab51b4e92bc098a176c8527bbff
Its0xyToan <0xytoanmail@gmail.com>
2025-04-21 17:08:22 -0500

    feat(): New parser

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -1,78 +1,50 @@
-/*
-    {
-        "customURL":"https://ticketywebhook.requestcatcher.com",
-        "guildId":"1161743005082992731",
-        "guildName":"Tickety Training",
-        "channelId":"1161743005082992734",
-        "userId":"503282932241268736",
-        "userName":"notdemonix",
-        "panel":"Support",
-        "type":"create",
-        "openDate":1706717176563
-    }
-*/
-
 import Events from "../objects/Events.js";
-import Priorities from "../objects/Priorities.js";
+import { priorityChange } from "../objects/Priorities.js";
 
-export default class Parser {
-    data;
-    parsed = {};
+export default class Parserv3 {
+  body;
 
-    constructor(data) {
-        this.data = data;
-        if(!data.type) return new Error("Unable to parse data because type is unexistant.");
+  constructor(body) {
+    this.body = body;
+  }
+
+  eventRegistry = {
+    "ticket.create": Events.Create,
+    "ticket.close": Events.Close,
+    "ticket.rename": Events.Rename,
+    "ticket.priority": Events.Priority,
+  };
+
+  work() {
+    const event = this.body["event"];
+    const payload = this.body["payload"];
+
+    if (!event || !payload) {
+      throw new Error("Invalid body (Using v3)");
     }
 
-    work () {
-        this.defaultParse()
-        switch(this.data.type) {
-            case Events.Create: this.parseCreate(); break;
-            case Events.Close: this.parseClose(); break;
-            case Events.Priority: this.parsePriority(); break;
-            case Events.Rename: this.parseRename(); break;
-        }
-
-        return this.parsed
+    let internalEvent = this.eventRegistry[event];
+    if (!internalEvent) {
+      throw new Error(`Unknown event: ${event}`);
     }
 
-    defaultParse() {
-        this.parsed = {
-            guild: {
-                id: this.data["guildId"],
-                name: this.data["guildName"]
-            },
-            channel: {
-                id: this.data["channelId"]
-            },
-            user: {
-                id: this.data["userId"],
-                name: this.data["userName"]
-            },
-            panel: this.data["panel"]
-        }
+    switch (internalEvent) {
+      case Events.Priority:
+        return {
+          type: internalEvent,
+          payload: this.parsePriority(payload),
+        };
+      default:
+        return {
+          type: internalEvent,
+          payload: payload,
+        };
     }
+  }
 
-    parseCreate () {
-        this.parsed["type"] = Events.Create
-        this.parsed["openTimestamp"] = this.data["openDate"]
-    }
-
-    parseClose () {
-        this.parsed["type"] = Events.Close
-    }
-
-    parseRename () {
-        this.parsed["type"] = Events.Rename
-        this.parsed["newName"] = this.data["newName"]
-    }
-
-    parsePriority () {
-        this.parsed["type"] = Events.Priority
-        this.parsed["priority"] = this.data["priority"] === 1 ?
-            Priorities.High : this.data["priority"] === 2 ?
-                    Priorities.Medium : Priorities.Low;
-    }
+  parsePriority = (payload) => {
+    payload["newPriority"] = priorityChange(payload["newPriority"]);
+    payload["oldPriority"] = priorityChange(payload["oldPriority"]);
+    return payload;
+  };
 }
-
-
