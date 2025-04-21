e9dbd3f0b0fb27d5787cde0bd88e2dce54aec956
Its0xyToan <0xytoanmail@gmail.com>
2025-04-21 17:09:08 -0500

    fix(client): Make client better

--- src/client.js
+++ src/client.js
@@ -1,47 +1,60 @@
-import { EventEmitter } from "node:events"
-import express from "express"
+import { EventEmitter } from "node:events";
+import express from "express";
 import Parser from "./parser/parser.js";
 import Events from "./objects/Events.js";
+import Parserv3 from "./parser/parser.js";
 
 /**
- * @typedef {object} ClientConfig;
- * @property {number?} port;
+ * @type {import("./index.d.ts").TicketyClient}
  */
-
-
 export default class TicketyClient extends EventEmitter {
-    /** @type {ClientConfig} */
-    config;
-    /** @type {import("express").Express} */
-    app;
+  config;
+  /** @type {import("express").Express} */
+  app;
 
-    /**
-     * Creates a new Tickety Client !
-     * @param {ClientConfig} config
-     */
-    constructor(config) {
-        super()
-        this.config = config
-    }
+  /**
+   * Creates a new Tickety Client !
+   * @param {import("./index.d.ts").Config} config
+   */
+  constructor(config) {
+    super();
+    this.config = config;
+  }
 
+  listen() {
+    if (!this.config.port) this.config.port = 3000;
 
-    listen () {
-        if(!this.config.port) this.config.port = 3000
+    this.app = express();
+    this.app.use(express.json());
 
-        this.app = express()
-        this.app.use(express.json())
+    this.app.post(`/${this.config.route ?? ""}`, (req, res) => {
+      if (!req.body)
+        return res
+          .status(400)
+          .json({ error: true, message: "Invalid form body, nice try bud" });
 
-        this.app.post("/tickety", (req, res) => {
-            if(!req.body) return res.status(400).json({ code: 400, error: true, message: "Invalid form body" });
-            
-            const parsed = new Parser(req.body).work()
-            this.emit(parsed.type, parsed)
-            this.emit(Events.Debug, req.body, parsed)
-            res.status(200).json({ code: 200, error: false })
-        })
+      if (req.headers.authorization !== this.config.key) {
+        return res
+          .status(401)
+          .json({ error: true, message: "Unauthorized, still nice try bud" });
+      }
 
-        this.app.listen(this.config.port, () => {
-            this.emit(Events.Ready, this.config.port)
-        })
-    }
+      const parsed = new Parserv3(req.body).work();
+
+      this.emit(parsed.type, parsed.payload);
+      this.emit(Events.Debug, req.body, parsed.payload);
+
+      res.status(200).json({
+        error: false,
+        message: "Hey from Tickety JS wrapper !",
+      });
+    });
+
+    this.app.listen(this.config.port, () => {
+      this.emit(Events.Ready, {
+        port: this.config.port || 3000,
+        route: "0.0.0.0/" + this.config.route ?? "",
+      });
+    });
+  }
 }
