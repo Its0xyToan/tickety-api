0905d09253ed65528698db6708891f46d20a154a
Its0xyToan <0xytoanmail@gmail.com>
2025-04-21 17:09:24 -0500

    feat(): Add changePriority function

--- src/objects/Priorities.js
+++ src/objects/Priorities.js
@@ -1,7 +1,15 @@
-
-
-export default class Priorities {
-    static High = "high"
-    static Medium = "medium"
-    static Low = "low"
-}
+export const priorityChange = (priority) => {
+  return {
+    number: priority,
+    toString: () => {
+      if (priority === 1) {
+        return "high";
+      } else if (priority === 2) {
+        return "medium";
+      } else if (priority === 3) {
+        return "low";
+      }
+      return "none";
+    },
+  };
+};
